44ef79fd56ba11dc8498edcfd265eaa8006811ec
Denis Ahrens <dennis.ahrens@mailbox.org>
2017-05-16 00:32:09 +0200

    Add install script for the plugins.

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if ! ( hash git 2>/dev/null )
+then
+    echo Please install git
+    exit 1
+fi
+
+if ! ( hash ctags 2>/dev/null )
+then
+    echo Please install ctags: apt install exuberant-ctags
+    exit 1
+fi
+
+declare -a PLUGINS=(
+    "https://github.com/tpope/vim-sensible"             # sensible
+    "https://github.com/tpope/vim-surround"             # wrap blocks
+    "https://github.com/tpope/vim-fugitive"             # git
+    "https://github.com/vim-airline/vim-airline"        # light status
+    "https://github.com/vim-syntastic/syntastic.git"    # syntax checks
+    "https://github.com/tomasr/molokai"                 # color schema
+    "https://github.com/kien/ctrlp.vim"                 # find mru, buf, f
+    "https://github.com/scrooloose/nerdtree"            # find files 
+    "https://github.com/majutsushi/tagbar"              # inspect files 
+)
+
+GIT_CLONE="git clone --depth 1"
+
+for URL in "${PLUGINS[@]}"
+do
+    PLUGIN=`printf -- "%s" "${URL##*/}"`
+    echo process plugin: $PLUGIN
+    if [ -d "./bundle/$PLUGIN" ]; then
+        echo Already installed
+    else
+        $GIT_CLONE $URL ./bundle/$PLUGIN
+    fi
+    #$GITCMD $plugin bundle/
+    # or do whatever with individual element of the array
+done
---
+++ vimrc
@@ -0,0 +1,11 @@
+execute pathogen#infect()
+filetype plugin indent on
+
+syntax on
+set nu
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+map <F2> :NERDTreeToggle<CR>
+map <F8> :TagbarToggle<CR>
