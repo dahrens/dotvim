cfb720796ff5b47713d5e3186144003cec143248
Denis Ahrens <dennis.ahrens@mailbox.org>
2017-05-16 01:22:52 +0200

    More plugins

---
+++ README.md
@@ -0,0 +1,12 @@
+# My vim config
+
+A collection of vim plugins together with my .vimrc
+
+## Install
+
+```bash
+cd ~
+mv .vim .vim.bak  # make a backup of your .vim folder if present
+git clone https://github.com/dahrens/vim.config.git .vim
+cd .vim && ./install.sh
+```
--- install.sh
+++ install.sh
@@ -17,12 +17,17 @@ declare -a PLUGINS=(
     "https://github.com/tpope/vim-surround"             # wrap blocks
     "https://github.com/tpope/vim-fugitive"             # git
     "https://github.com/vim-airline/vim-airline"        # light status
-    "https://github.com/vim-syntastic/syntastic.git"    # syntax checks
+    "https://github.com/vim-syntastic/syntastic"        # syntax checks
     "https://github.com/tomasr/molokai"                 # color schema
     "https://github.com/kien/ctrlp.vim"                 # find mru, buf, f
     "https://github.com/scrooloose/nerdtree"            # find files 
-    "https://github.com/majutsushi/tagbar"              # inspect files 
+    "https://github.com/majutsushi/tagbar"              # inspect files
+    "https://github.com/ervandew/supertab"              # autocomplete tab
+    "https://github.com/plasticboy/vim-markdown"        # markdown
+    "https://github.com/pangloss/vim-javascript"        # javascript
 )
+
+
 
 GIT_CLONE="git clone --depth 1"
 
@@ -38,3 +43,19 @@ do
     #$GITCMD $plugin bundle/
     # or do whatever with individual element of the array
 done
+
+VIMRC=~/.vimrc
+
+if [ -f $VIMRC ]
+then
+    if [ -L $VIMRC ]
+    then
+        echo Already symlinked
+    else
+        echo backup your .vimrc
+        mv $VIMRC $VIMRC.bak
+        echo symlink .vim/vimrc
+        ln -s ~/.vim/vimrc $VIMRC
+    fi
+fi
+
